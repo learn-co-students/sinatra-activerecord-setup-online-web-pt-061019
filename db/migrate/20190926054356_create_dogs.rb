1dd2054748a6bed96aac1d2d4ef4fc6507e7cd9c
ZumbaIdol <david.a.foote@gmail.com>
2019-09-26 15:18:39 -0500

    Done.

--- db/migrate/20190926054356_create_dogs.rb
+++ db/migrate/20190926054356_create_dogs.rb
@@ -6,16 +6,14 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   #   end
 
   def up
-    create_table do |t|
+    create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
   end
 
-
- 
   def down
-    drop_table
+    drop_table :dogs
   end
 end
   
